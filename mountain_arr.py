909559e3aabe305f76a8a670ff9d29cdcd01a3e6
Alice_Suh <dongeuns33@gmail.com>
2022-10-02 12:27:44 -0700

    10022022

--- mountain_arr.py
+++ mountain_arr.py
@@ -1,10 +1,5 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        i = 0 
-        index_pos = 0 
-        while(i < len(arr) -1):
-            if arr[i] < arr[i + 1]:
-                index_pos += 1
-            else:
-                return index_pos
-            i += 1
+        for i, val in enumerate(arr):
+            if arr[i + 1] < arr[i]:
+                return i
