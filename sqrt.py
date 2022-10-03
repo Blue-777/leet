ed96b71ce951b75274dc357e67921111e1b8212d
Alice_Suh <dongeuns33@gmail.com>
2022-10-02 20:50:09 -0700

    10022022

--- sqrt.py
+++ sqrt.py
@@ -1,5 +1,14 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        import math 
+        l = 0
+        r = x
+
+        while l + 1 < r:
+            m = (l+r)//2
+            if m * m > x: r = m 
+            if m * m < x: l = m
+            if m * m == x: return int(m)
+
+        if r * r == x: return int(r)
+        return int((l+r)/2)
         
-        return int(math.sqrt(x))
